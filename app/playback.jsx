2cf2e6d9b467b400cbfb3217eff9a33c478e8b26
Darko Mikanovic <darko.mikanovic7@gmail.com>
2026-04-13 14:00:12 +0200

    V1.0.5: Added recommended sequels on play button page, and skip to next scene on playback page

--- app/playback.jsx
+++ app/playback.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { useData } from "../hooks/useData";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { router } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 import Video from "react-native-video";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -29,6 +30,22 @@ const Playback = () => {
 
     const [currentSource, setCurrentSource] = useState(sourcePri);
     const [videoKey, setVideoKey] = useState(0);
+    const [showSkip, setShowSkip] = useState(-1);
+    const [skipSegmentStart, ] = useState(selectedFilm.Uri !== "" ? (
+        selectedFilm.SkipSegments !== null && selectedFilm.SkipSegments.length > 0 ? selectedFilm.SkipSegments.map((val) => val.substring(0, 1) * 3600 + val.substring(2, 4) * 60 + Number(val.substring(5, 7))) : []
+    ) : (
+        selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments !== null && selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments.length > 0 ? selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments.map((val) => val.substring(0, 1) * 3600 + val.substring(2, 4) * 60 + Number(val.substring(5, 7))) : []
+    ));
+    const [skipSegmentEnd, ] = useState(selectedFilm.Uri !== "" ? (
+        selectedFilm.SkipSegments !== null && selectedFilm.SkipSegments.length > 0 ? selectedFilm.SkipSegments.map((val) => val.substring(8, 9) * 3600 + val.substring(10, 12) * 60 + Number(val.substring(13, 15))) : []
+    ) : (
+        selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments !== null && selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments.length > 0 ? selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].SkipSegments.map((val) => val.substring(8, 9) * 3600 + val.substring(10, 12) * 60 + Number(val.substring(13, 15))) : []
+    ));
+
+    const handleSkip = useCallback(() => {
+        if (showSkip > -1 && showSkip < skipSegmentEnd.length)
+            playerRef.current.seek(skipSegmentEnd[showSkip]);
+    }, [showSkip, skipSegmentEnd]);
 
     return (
         <View style={{ flex: 1, backgroundColor: "black", justifyContent: "center", alignItems: "center" }}>
@@ -61,7 +78,15 @@ const Playback = () => {
                 onProgress={async e => {
                     let timestamp = Math.round(e.currentTime);
                     if (timestamp % 300 === 0 && timestamp > 0)
-                        await AsyncStorage.setItem(selectedFilm.Uri !== "" ? selectedFilm.Id : selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].Id, JSON.stringify(timestamp), () => console.log((selectedFilm.Uri !== "" ? selectedFilm.Id : selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].Id) + "  " + timestamp));
+                        await AsyncStorage.setItem(selectedFilm.Uri !== "" ? selectedFilm.Id : selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].Id, JSON.stringify(timestamp));
+                    if (skipSegmentStart.length > 0 && timestamp % 2 === 0)
+                    {
+                        let skipIndex = skipSegmentStart.findIndex(s => timestamp >= s);
+                        if (skipIndex > -1 && timestamp < skipSegmentEnd[skipIndex])
+                            setShowSkip(skipIndex);
+                        else
+                            setShowSkip(-1);
+                    }
                 }}
                 controls
                 paused={false}
@@ -70,6 +95,12 @@ const Playback = () => {
                 playWhenInactive={false}
                 ignoreSilentSwitch="ignore"
             />
+            {showSkip > -1 && <Pressable style={[styles.btn, { width: "20%", position: "absolute", right: "10", bottom: "100" }]}
+                onFocus={(e) => e.currentTarget.setNativeProps({style: { backgroundColor: "#0004ff" }})}
+                onBlur={(e) => e.currentTarget.setNativeProps({style: { backgroundColor: "black" }})}
+                onPress={() => { handleSkip(); }}>
+                <FontAwesome name="forward" style={[styles.title, styles.textFocused]}> Do sledeće scene</FontAwesome>
+            </Pressable>}
         </View>
     );
 }
@@ -78,22 +109,34 @@ export default Playback;
 
 const styles = StyleSheet.create({
     title: {
+        textAlign: "center",
         fontWeight: "bold",
-        fontSize: 22,
-        marginBottom: 10
+        fontSize: 18,
+        color: "#50ffd9"
     },
     btn: {
-        backgroundColor: "#0022ffff",
-        padding: 15,
-        borderRadius: 6,
-        marginVertical: 10,
-        boxShadow: "0px 4px rgba(0,0,0,0.2)"
+        backgroundColor: "#000000",
+        padding: 10,
+        margin: 5,
+        marginBottom: 0,
+        borderWidth: 3,
+        borderColor: "#6600ff"
     },
-    pressed: {
-        opacity: 0.6
+    btnh: {
+        backgroundColor: "#000000",
+        padding: 5,
+        margin: 5,
+        marginRight: 0,
+        borderWidth: 3,
+        borderColor: "#6600ff"
     },
-    cover: {
-        width: "50%",
-        height: "100%"
+    textFocused: {
+        color: "#ffdd00"
+    },
+    focused: {
+        backgroundColor: "#0004ff"
+    },
+    selected: {
+        backgroundColor: "#6600ff"
     }
 });
--- app/preview/playButton.jsx
+++ app/preview/playButton.jsx
@@ -1,12 +1,12 @@
-import { ActivityIndicator, Pressable, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Pressable, StyleSheet, Text, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useData } from "../../hooks/useData";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const PlayButton = () => {
-    const { selectedFilm, selectedSeason, selectedEpisode, ensureEmbedded, setSeekTimestamp } = useData();
+    const { videos, selectedFilm, selectedSeason, selectedEpisode, search, setSelectedFilm, setSelectedSeason, ensureEmbedded, setSeekTimestamp } = useData();
     const [loading, setLoading] = useState(false);
     const [timestamp, setTimestamp] = useState(1);
 
@@ -21,6 +21,21 @@ const PlayButton = () => {
         router.push("/playback");
     };
 
+    const [nextTitle, setNextTitle] = useState(null);
+
+    const handleNext = useCallback((film) => {
+        setSelectedFilm(film);
+        if (film.Series.length === 0)
+            router.replace("/preview/playButton");
+        else if (film.Series.length === 1)
+        {
+            setSelectedSeason(1);
+            router.replace("/preview/episodes");
+        }
+        else if (film.Series.length > 1)
+            router.replace("/preview/seasons");
+    }, []);
+
     useEffect(() => {
         const checkExistingTimestamp = async () => {
             let res = await AsyncStorage.getItem(selectedFilm.Uri !== "" ? selectedFilm.Id : selectedFilm.Series[selectedSeason - 1][selectedEpisode - 1].Id);
@@ -28,8 +43,30 @@ const PlayButton = () => {
                 setTimestamp(Number(res));
             else
                 setTimestamp(0);
-        }
+        };
         checkExistingTimestamp();
+        const autoCheckExistingTimestamp = setInterval(checkExistingTimestamp, 2000);
+
+        const checkNextTitle = async () => {
+            if (selectedFilm.Uri !== "" || (selectedFilm.Series.length > 0 && selectedFilm.Series.length === selectedSeason && selectedFilm.Series[selectedSeason - 1].length === selectedEpisode))
+            {
+                let next = videos.find(v => v.PreviousTitle === selectedFilm.Title);
+                if (next !== undefined)
+                    setNextTitle(next);
+                else
+                {
+                    await search("");
+                    setTimeout(() => {
+                        let next = videos.find(v => v.PreviousTitle === selectedFilm.Title);
+                        if (next !== undefined)
+                            setNextTitle(next);
+                    }, 1000);
+                }
+            }
+        }
+        checkNextTitle();
+
+        return () => { clearInterval(autoCheckExistingTimestamp); };
     }, [selectedFilm, selectedSeason, selectedEpisode]);
 
     return (
@@ -48,6 +85,14 @@ const PlayButton = () => {
                 disabled={loading}>
                 <FontAwesome name="play" style={[styles.title, styles.textFocused]}>{timestamp > 0 ? "  Od početka" : ""}</FontAwesome>
             </Pressable>
+            {nextTitle !== null && <Pressable style={[styles.btn, { width: "30%", position: "absolute", right: "10" }, loading && { opacity: 0.4 }]}
+                onFocus={(e) => e.currentTarget.setNativeProps({style: { backgroundColor: "#0004ff" }})}
+                onBlur={(e) => e.currentTarget.setNativeProps({style: { backgroundColor: "black" }})}
+                onPress={() => { handleNext(nextTitle); }}
+                disabled={loading}>
+                <FontAwesome name="arrow-right" style={[styles.title]}> Preporučen nastavak:</FontAwesome>
+                <Text style={[styles.title, styles.textFocused]}> {nextTitle.Title} ({nextTitle.Year})</Text>
+            </Pressable>}
             {loading && <ActivityIndicator style={{ marginTop: 20 }} size="large" color="white" />}
         </View>
     );
